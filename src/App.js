b4f434bb548e8198a0a37067d855488a88a68fd9
Eric Wang <72224088+Eric7398@users.noreply.github.com>
2022-04-18 22:35:11 -0700

    temp complete build 4/18

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import ScrollToTop from './components/ScrollTop';
 
 import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
-import Price from "./components/price/Price";
-import Rules from './components/rules/Rules.jsx';
+// import Navbar from "./components/navbar/Navbar";
+// import About from "./components/about/About";
+// import Contact from "./components/contact/Contact";
+// import Price from "./components/price/Price";
+// import Rules from './components/rules/Rules.jsx';
 import NotFound from './components/404/NotFound';
 
 
@@ -16,15 +16,15 @@ function App() {
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <ScrollToTop />
       <Routes key={location.pathname} location={location}>
 
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} />
         <Route path="/price" element={<Price />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/rules" element={<Rules />} />
+        <Route path="/rules" element={<Rules />} /> */}
         <Route path="*" element={<NotFound />} />
 
       </Routes>
